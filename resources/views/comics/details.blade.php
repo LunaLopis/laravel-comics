0adb71542402a018663811633ef61fef0f6ab64b
Luna Lopis <lunalopis59@gmail.com>
2023-07-26 15:30:39 +0200

    dettagli stampati

--- resources/views/comics/details.blade.php
+++ resources/views/comics/details.blade.php
@@ -3,6 +3,23 @@
 @section('content')
 
 <div>
-    dettagli
+    <h1 class="text-primary">{{$comic['title']}}</h1>
+    <h3>{{$comic['type']}}</h3>
+    <h3>{{$comic['description']}}</h3>
+    <h3>{{$comic['price']}}</h3>
+    <div class="d-flex">
+        <h2>art by:</h2>
+        @foreach($comic['artists'] as $key => $artist)
+        <span> {{$artist}},</span>
+        @endforeach
+    </div>
+    <div class="d-flex">
+        <h2>written by:</h2>
+        @foreach($comic['writers'] as $key => $writer)
+        <span> {{$writer}},</span>
+        @endforeach
+    </div>
+
 </div>
+
 @endsection
